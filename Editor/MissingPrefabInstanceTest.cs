ffbadd52cfd2d2719fc1a2b2f935b0cbf0f2a7db
baba_s <shotababa1206@gmail.com>
2020-09-25 21:18:53 +0900

    first commit

---
+++ Editor/MissingPrefabInstanceTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class MissingPrefabInstanceTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Missing_Prefab_になっているゲームオブジェクトが存在しない()
+		{
+			AllGameObjectTester.Test( gameObject => PrefabUtility.GetPrefabInstanceStatus( gameObject ) != PrefabInstanceStatus.MissingAsset );
+		}
+	}
+}
